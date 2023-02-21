079c446838f69c5fc8d43028aaee37761e33658f
Peter Atef <peter.atef@coformatique.com>
2023-02-21 19:38:43 +0200

    added get-min function

--- main.cpp
+++ main.cpp
@@ -10,9 +10,18 @@ int getSum(vector<int> arr){
     return s;
 }
 
+int getMin(vector<int> arr){
+    int s = arr[0];
+    for (int element: arr){
+        s = (s > element)? element: s;
+    }
+    return s;
+}
+
 int main(int argc, char const *argv[])
 {
-    vector<int> arr{1,2, 3, 4, 5};
+    vector<int> arr{1, 2, 3, 4, 7};
     cout << getSum(arr) << endl;
+    cout << getMin(arr) << endl;
     return 0;
 }
